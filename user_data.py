cc117e1df6392d7072f1705a5201f4d4fed05f29
YongwangWu <wu_yongwang@163.com>
2022-04-18 16:40:30 +0800

    add class of user_data; override str method

--- user_data.py
+++ user_data.py
@@ -6,3 +6,8 @@ class User(None):
 
     def __str__(self):
         return "{0:<10}:{1:<10},{2:<10}".format(self.name, self.sex, self.age)
+
+    def __le__(self, other):
+        if not other.isinstance(User):
+            return None
+        return self.age < other.age
