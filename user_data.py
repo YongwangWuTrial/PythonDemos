432b96726602e8331329c90b184e1e6d9bd3979a
YongwangWu <wu_yongwang@163.com>
2022-04-18 15:59:35 +0800

    add class of user_data; override str method

--- user_data.py
+++ user_data.py
@@ -1,6 +1,8 @@
-
 class User(None):
     def __init__(self, name="", age=20, sex="Male"):
         self.name = name
         self.age = age
         self.sex = sex
+
+    def __str__(self):
+        return "{0:<10}:{1:<10},{2:<10}".format(self.name, self.sex, self.age)
